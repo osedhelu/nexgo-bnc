1cabb40b00f0ead87ce0f5725f336923a9ecc04b
osedhelu <osedhelu@gmail.com>
2024-11-18 19:09:18 -0500

    test(TLV): verificacion de formato tlv

--- app/src/main/java/com/osedhelu/bnc/utils/FormateTLV.kt
+++ app/src/main/java/com/osedhelu/bnc/utils/FormateTLV.kt
@@ -1,29 +1,91 @@
-fun decodeTLV(data: ByteArray): TLV {
-    val tagLength = if (data[0].toInt() and 0x1F == 0x1F) 2 else 1
-    val tag = data.take(tagLength).joinToString("") { String.format("%02X", it) }
-    val lengthByte = data[tagLength].toInt()
-    val length: Int
-    val valueStartIndex: Int
+package com.credibanco.smartpos.payment.utils
 
-    if (tag == "C2") {
-        // Validación específica para el tag C2
-        if (lengthByte and 0x80 == 0x80 && (lengthByte and 0x7F) == 2) {
-            length = 0x0178 // 376 bytes
-            valueStartIndex = tagLength + 1 + 2 // 2 bytes for length
-        } else {
-            throw IllegalArgumentException("Invalid length for tag C2")
-        }
-    } else if (lengthByte and 0x80 == 0x80) {
-        val numLengthBytes = lengthByte and 0x7F
-        length = ByteBuffer.wrap(data, tagLength + 1, numLengthBytes).int
-        valueStartIndex = tagLength + 1 + numLengthBytes
-    } else {
-        length = lengthByte
-        valueStartIndex = tagLength + 1
+data class TLV(val tag: String, val length: String, val value: String)
+
+fun getLength(classValue: String): Int {
+    return when (classValue) {
+        "83" -> 8
+        else -> 6
+    }
+}
+
+fun getTlV(data: String, len: Int, lenCode: Int): TLV {
+    val TagName = data.substring(0, len)
+    val lenHex = TagName.substring(TagName.length - lenCode)
+    println("TagNama ===== $TagName")
+    println("lenHex ===== $lenHex")
+
+    val value = data.substring(TagName.length, TagName.length + (lenHex.toInt(16) * 2))
+
+    return TLV(TagName.dropLast(2), lenHex, value)
+}
+
+fun getTLVClass(data: String, lenCode: String, len: Int): TLV {
+    val initTag = data.substring(0, 2) + data.substring(2, len)
+    val allTagAndValue = initTag + lenCode
+    val value =
+            data.substring(allTagAndValue.length, allTagAndValue.length + (lenCode.toInt(16) * 2))
+    return TLV(initTag, lenCode, value)
+}
+
+fun decodeTLVList(data: String): List<TLV> {
+    val tlvs = mutableListOf<TLV>()
+    var i = 0
+    var tmpIndex = 0
+
+    while (i < data.length) {
+        tmpIndex += 1
+        val label = data.substring(i, i + 2)
+        val tagState: TLV =
+                when (label) {
+                    "C0" -> {
+                        val newData = data.substring(i, data.length)
+                        val lenCode = newData.substring(2, 4)
+                        getTLVClass(newData, lenCode, 2)
+                    }
+                    "C1" -> {
+                        val newData = data.substring(i, data.length)
+                        val lenCode = newData.substring(2, 4)
+                        getTLVClass(newData, lenCode, 2)
+                    }
+                    "C2" -> {
+                        val newData = data.substring(i, data.length)
+                        val lenCode = newData.substring(4, 8)
+                        getTLVClass(newData, lenCode, 4)
+                    }
+                    "C4" -> {
+                        val newData = data.substring(i, data.length)
+                        val lenCode = newData.substring(2, 4)
+                        getTLVClass(newData, lenCode, 2)
+                    }
+                    "9F" -> {
+                        val newData = data.substring(i, data.length)
+                        val lenCode = newData.substring(2, 4)
+                        val a = getLength(lenCode)
+                        getTlV(newData, a, 2)
+                    }
+                    "DF" -> {
+                        val newData = data.substring(i, data.length)
+                        val lenCode = newData.substring(2, 4)
+                        val a = getLength(lenCode)
+                        getTlV(newData, a, 2)
+                    }
+                    else -> {
+                        val newData = data.substring(i, data.length)
+                        val lenCode = newData.substring(2, 4)
+                        val a = getLength(lenCode)
+                        getTlV(data, a, 2)
+                    }
+                }
+        tlvs.add(tagState)
+        val dataState = "${tagState.tag}${tagState.length}${tagState.value}"
+        println("xxxxxxxx $dataState")
+        i += dataState.length
     }
 
-    val value =
-        data.drop(valueStartIndex).take(length).joinToString("") { String.format("%02X", it) }
+    return tlvs
+}
 
-    return TLV(tag, length, value)
+fun TestFormateTLV(hash: String): List<TLV> {
+    return decodeTLVList(hash)
 }
